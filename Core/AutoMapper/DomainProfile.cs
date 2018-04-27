e3d844b1d79c1a930ddd4f30ffff36a0c71d58fb
Marius Kalvø <mariuskalvo30@gmail.com>
2018-04-27 19:56:51 +0200

    Automapper config for Thread til ThreadEntry

--- Core/AutoMapper/DomainProfile.cs
+++ Core/AutoMapper/DomainProfile.cs
@@ -17,6 +17,9 @@ namespace Core.AutoMapper
 
             CreateMap<CreateGroupThreadDto, GroupThread>();
             CreateMap<GroupThread, GroupThreadDto>().ReverseMap();
+
+            CreateMap<CreateThreadEntryDto, ThreadEntry>();
+            CreateMap<ThreadEntry, ThreadEntryDto>().ReverseMap();
         }
     }
 }
