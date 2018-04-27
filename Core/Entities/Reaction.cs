de77e1141cba0920440a82952ec6073e4e132e64
Marius Kalvø <mariuskalvo30@gmail.com>
2018-04-28 00:02:00 +0200

    La til Reaction-entitet

--- Core/Entities/Reaction.cs
+++ Core/Entities/Reaction.cs
@@ -6,19 +6,14 @@ using System.Text;
 
 namespace Core.Entities
 {
-   public  class ThreadEntry
+    public class Reaction
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
-        public long GroupThreadId { get; set; }
-        public GroupThread Thread { get; set; }
-
-        public string Content { get; set; }
-        public IEnumerable<Reaction> Reactions { get; set; }
-
-        public DateTime CreatedAt { get; set; }
-
+        public ThreadEntry ThreadEntry { get; set; }
+        public long ThreadEntryId { get; set; }
+        public string ReactionId { get; set; }
     }
 }
