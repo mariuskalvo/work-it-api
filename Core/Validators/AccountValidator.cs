836e4a20073ddd7b9d5a4d7e2f89c690eea63371
Marius Kalvø <mariuskalvo30@gmail.com>
2018-05-04 17:22:28 +0200

    Added account validator

---
+++ Core/Validators/AccountValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.DTOs;
+using FluentValidation;
+
+namespace Core.Validators
+{
+    class AccountValidator : AbstractValidator<CreateAccountDto>
+    {
+        public AccountValidator()
+        {
+            RuleFor(a => a.Password.Equals(a.RepeatPassword));
+        }
+    }
+}
