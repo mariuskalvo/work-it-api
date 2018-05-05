49b092c25b12dc104f3db677c56a116768bc0426
Marius Kalvø <mariuskalvo30@gmail.com>
2018-05-05 18:41:53 +0200

    Added AccountController for adding accounts

---
+++ WebApi/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.DTOs;
+using Core.Services.Interfaces;
+using Core.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+
+        [HttpPost]
+        public async Task<ValidationResponse> CreateAccount(CreateAccountDto createAccountDto)
+        {
+            var validationResult = await accountService.CreateAccount(createAccountDto);
+            return validationResult;
+        }
+    }
+}
